d9028b5f374c36c15284a5ce3a6e26fbbdcc10c2
Mike Brewer <mikey.brew@gmail.com>
2014-10-28 13:32:03 -0500

    Completed last API piece
    Done

--- coords_to_weather.rb
+++ coords_to_weather.rb
@@ -4,8 +4,8 @@ require 'json'
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
 
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 puts "Let's get the weather forecast for your location."
 
@@ -19,8 +19,16 @@ the_longitude = gets.chomp
 #   address_to_coords.rb to read from a remote API and parse
 #   the results.
 
+weather_url = "https://api.forecast.io/forecast/23f3953b05f926b4064525e3392e120f/" + the_latitude.to_s + "," + the_longitude.to_s
+currentforecast_raw = open(weather_url).read
+parsed_weatherdata = JSON.parse(currentforecast_raw)
+
+the_temperature = parsed_weatherdata["currently"]["temperature"]
+the_hour_outlook = parsed_weatherdata["hourly"]["data"][1]["summary"]
+the_day_outlook = parsed_weatherdata["daily"]["data"][1]["summary"]
+
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
